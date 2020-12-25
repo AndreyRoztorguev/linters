09b1a98f843a7694ff8d497224c1887b7db4fca4
Andrey Roztorguev <andrew.roztor99@gmail.com>
2020-12-25 16:01:12 +0200

    webpack-react-eslint-prettier-vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,29 @@
 module.exports = {
+    root: true,
     parserOptions: {
-        ecmaVersion: 6,
-        sourceType: "module",
+        ecmaVersion: 2020,
+        sourceType: 'module', // Allows using import/export statements
         ecmaFeatures: {
-            jsx: true
+            jsx: true // Enable JSX since we're using React
         }
     },
+    env: {
+        browser: true, // Enables browser globals like window and document
+        amd: true, // Enables require() and define() as global variables as per the amd spec.
+        node: true // Enables Node.js global variables and Node.js scoping.
+    },
+    settings: {
+        react: {
+            version: 'detect' // Automatically detect the react version
+        }
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
+    ],
     rules: {
-        semi: ["warn", "always"]
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }] // Use our .prettierrc file as source
     }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: ['./src/index.js'],
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
-    
+
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
-                use: "babel-loader",
+                use: 'babel-loader'
                 // resolve: {
                 //     extensions: [".js", ".jsx"]
-                // }, 
+                // },
             },
             {
-                test:/\.css$/,
+                test: /\.css$/,
                 use: [
                     {
-                       loader: MiniCssExtractPlugin.loader,
+                        loader: MiniCssExtractPlugin.loader
                     },
-                    "css-loader",
+                    'css-loader'
                 ],
-                exclude: /node_modules/,
-            },
+                exclude: /node_modules/
+            }
         ]
     },
     resolve: {
         modules: ['node_modules'],
-        extensions: ['.js','.jsx']
+        extensions: ['.js', '.jsx']
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
         new MiniCssExtractPlugin({
-            filename: "main.css",
+            filename: 'main.css'
         })
     ],
     devServer: {
         port: 8081,
-        open: true,
+        open: true
     }
 };
